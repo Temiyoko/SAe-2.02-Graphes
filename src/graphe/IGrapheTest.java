8cf88d559dba87ba79201b812af3edfb7711cd8c
justfreezo <pisraelmartins@gmail.com>
2023-05-03 20:22:24 +0200

    Update du fichier IGrapheTest pour la partie 2 et 3 ainsi que l'ajout du fichier Main et le PDF Perfs.

--- src/graphe/IGrapheTest.java
+++ src/graphe/IGrapheTest.java
@@ -4,20 +4,30 @@ package graphe;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Test;
-import org.junit.platform.engine.support.descriptor.FileSystemSource;
 
 class IGrapheTest {
-	private IGraphe[] graphes = { 
+	private final IGraphe[] graphes = { 
 			new GrapheLArcs(), new GrapheLAdj(),
 			new GrapheMAdj(), new GrapheHHAdj()
 	};
+	
 	// graphe de l'exercice 3.1 du poly de maths
 	// avec en plus un noeud isole : J
-	private String g31 = 
+	private final String g31 = 
 			"A-C(2), A-D(1), "
 			+ "B-G(3), "
 			+ "C-H(2), "
@@ -29,7 +39,7 @@ class IGrapheTest {
 			+ "I-H(10), "
 			+ "J:";
 	
-	private String g31a = ""       // arcs non tries
+	private final String g31a = ""       // arcs non tries
 			+ "D-C(5), D-E(3), D-B(3), "
 			+ "E-G(3), E-C(1), E-H(7), "
 			+ "I-H(10), "
@@ -77,8 +87,8 @@ class IGrapheTest {
 				() -> g.ajouterArc("A", "B", -1)); // valuation negative
 	}
 	
-	void testImportation(IGraphe g) {
-		Arc a = GraphImporter.importer("graphes/ac/g-10-1.txt", g);
+	void petiteImporation(IGraphe g) {
+		Arc a = GraphImporter.importer("graphes/g-10-1.txt", g);
 		assertEquals("1-3(5), "
 				+ "10-3(3), 2-1(5), 2-3(5), 2-5(4), "
 				+ "3-4(4), 3-5(4), 4-10(1), 4-2(1), 4-7(3), "
@@ -89,10 +99,80 @@ class IGrapheTest {
 		assertEquals("7", a.getDestination());		
 	}
 	
-	@Test
-	void limporter() throws NumberFormatException, FileNotFoundException {
+	//@Test
+	void petitTestImportation() {
 		for (IGraphe g : graphes)
-			testImportation(g);		
+			petiteImporation(g);	
 	}
+	
+	@Test
+	void importer() throws NumberFormatException, FileNotFoundException {
+		String graphesRep = "graphes"; 
+        String reponsesRep = "reponses"; 
+        try {
+            List<Path> files1 = Files.list(Paths.get(graphesRep))
+                                     .filter(Files::isRegularFile)
+                                     .collect(Collectors.toList());
+            List<Path> files2 = Files.list(Paths.get(reponsesRep))
+                                     .filter(Files::isRegularFile)
+                                     .collect(Collectors.toList());
+
+            Iterator<Path> iterator1 = files1.iterator();
+            Iterator<Path> iterator2 = files2.iterator();
+
+            while (iterator1.hasNext() && iterator2.hasNext()) {
+                Path file1 = iterator1.next();
+                Path file2 = iterator2.next();
+
+                IGraphe g = new GrapheHHAdj(); 
+                Arc arc = GraphImporter.importer(file1.toFile(), g);
+
+                List<Integer> listeEntiers = new ArrayList<>();
+                int distance_attendue = GraphImporter.importerReponse(file2.toString(), listeEntiers);
+
+                System.out.println("\ngraphe: " + file1.getFileName());
+                System.out.println("chemin a trouver " + arc.getSource()+ " "+ arc.getDestination());
+                System.out.println("Graphe de  " + g.getSommets().size() + " sommets");
+
+                System.out.println("reponse: " + file2.getFileName());
+                if (distance_attendue >=0) {
+                	System.out.println("distance attendue : " + distance_attendue);
+                	System.out.println("chemin possible : " + listeEntiers);
+                } else System.out.println("Aucun chemin attendu");
+                
+                Map<String, Integer> dist = new HashMap<>();
+                Map<String, String> prev = new HashMap<>();
+                long debut = System.nanoTime();
+                Dijkstra.dijkstra(g, arc.getSource(), dist, prev);
+                long fin = System.nanoTime();
+                System.out.println("dijkstra a dure " + (fin - debut)/1000000 + " millisecondes");
+                
+                // reconstruction d'un plus court chemin
+                List<String> path = new LinkedList<>();
+                String currentNode = arc.getDestination();
+                while (currentNode != null && !currentNode.equals(arc.getSource())) {
+                    path.add(0, currentNode);
+                    currentNode = prev.get(currentNode);
+                }
+
+                // ajouter la source
+                if (currentNode != null && currentNode.equals(arc.getSource())) {
+                    path.add(0, arc.getSource());
+                }
+
+                if (dist.containsKey(arc.getDestination())) {
+                    int distance_trouvee = dist.get(arc.getDestination());
+                	System.out.println("chemin trouve : <" + String.join(", ", path)+">");
+                	System.out.println("Distance trouvee : " + distance_trouvee);
+                	assertEquals(distance_trouvee, distance_attendue);
+                } else {
+                	System.out.println("Aucun chemin trouve");
+                	assertEquals(-1, distance_attendue);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Erreur lors de l'acces aux dossiers: " + e.getMessage());
+        }
+    }
 
 }
