0e0cfb26032bc68a9c669d1419e65aa2960f22c3
Yukiwi <117077762+Temiyoko@users.noreply.github.com>
2023-05-17 16:10:40 +0200

    Cleaned up structure

---
+++ src/main/java/graphe/algos/Dijkstra.java
@@ -0,0 +1,10 @@
+package main.java.graphe.algos;
+
+import main.java.graphe.core.IGrapheConst;
+
+import java.util.Map;
+
+public class Dijkstra {
+    public static void dijkstra(IGrapheConst g, String source, Map<String, Integer> dist, Map<String, String> prev) {
+    }
+}
--- src/main/java/graphe/algos/DijkstraTools.java
+++ src/main/java/graphe/algos/DijkstraTools.java
@@ -1,9 +1,7 @@
 package main.java.graphe.algos;
 
-import main.java.graphe.core.IGraphe;
 import main.java.graphe.core.IGrapheConst;
 
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
